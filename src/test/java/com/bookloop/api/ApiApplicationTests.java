f0dc2f1420d6e462850dc528350c923e74e2221a
03caique <106028269+03caique@users.noreply.github.com>
2026-04-24 11:08:36 -0300

    feat: setup inicial do Spring Boot 3 com Java 21 e MySQL

---
+++ src/test/java/com/bookloop/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookloop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
